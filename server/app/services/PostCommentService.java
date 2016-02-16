c117f14ed587228409319f376d8d7668dab7dd95
James Tayor <j.taylor@kainos.com>
2016-02-16 15:00:10 +0000

    Remove unused methods

--- server/app/services/PostCommentService.java
+++ server/app/services/PostCommentService.java
@@ -11,20 +11,6 @@ public class PostCommentService {
     public static final Model.Finder<Long, PostComment> find = new Model.Finder<Long, PostComment>(
             Long.class, PostComment.class);
 
-    public static List<PostComment> findAllCommentsByPost(final BlogPost blogPost) {
-        return find
-                .where()
-                .eq("post", blogPost)
-                .findList();
-    }
-
-    public static List<PostComment> findAllCommentsByUser(final User user) {
-        return find
-                .where()
-                .eq("user", user)
-                .findList();
-    }
-
     public static PostComment findCommentById(final long id) {
         return find
                 .where()
